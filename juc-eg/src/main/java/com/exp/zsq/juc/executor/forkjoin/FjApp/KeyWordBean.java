a596faf2620c7ed2d9b4ab10e042d65a19ad7ea5
zhaoshengqi <zhaoshengqi@passiontec.com>
2018-02-22 18:27:36 +0800

    -------ForkJoinPool application------

---
+++ juc-eg/src/main/java/com/exp/zsq/juc/executor/forkjoin/FjApp/KeyWordBean.java
@@ -0,0 +1,47 @@
+package com.exp.zsq.juc.executor.forkjoin.FjApp;
+
+/**
+ * Created by zhaoshengqi on 2018/2/22.
+ */
+public class KeyWordBean {
+
+    private String id;
+
+    private String disease_name;
+
+    private String sex;
+
+    private String map_keys;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisease_name() {
+        return disease_name;
+    }
+
+    public void setDisease_name(String disease_name) {
+        this.disease_name = disease_name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getMap_keys() {
+        return map_keys;
+    }
+
+    public void setMap_keys(String map_keys) {
+        this.map_keys = map_keys;
+    }
+}
