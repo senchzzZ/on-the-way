9792a9dfb7839b57f2447308d5e30d8b59590319
zhaoshengqi <zhaoshengqi@huobi.com>
2021-03-19 09:54:38 +0800

    test gitee sync

---
+++ juc-eg/src/main/java/com/exp/zsq/leetcode/ParkingSystem.java
@@ -0,0 +1,35 @@
+package com.exp.zsq.leetcode;
+
+/**
+ * @author zhaoshengqi
+ * @date 2021/3/19 9:48 上午
+ */
+public class ParkingSystem {
+    int big,medium,small;
+    ParkingSystem(int big, int medium, int small) {
+        this.big = big;
+        this.medium = medium;
+        this.small = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (carType == 1) {
+            if (big > 0) {
+                big--;
+                return true;
+            }
+        } else if (carType == 2) {
+            if (medium > 0) {
+                medium--;
+                return true;
+            }
+        } else if (carType == 3) {
+            if (small > 0) {
+                small--;
+                return true;
+            }
+        }
+        return false;
+
+    }
+}
